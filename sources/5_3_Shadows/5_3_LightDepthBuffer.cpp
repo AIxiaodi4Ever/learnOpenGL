6415863d09cd0e87db0db2b52e94cc0839624d40
AIxiaodi <feifanplus@163.com>
2023-12-13 23:16:55 +0800

    add shadow map in perspective matrix

--- sources/5_3_Shadows/5_3_LightDepthBuffer.cpp
+++ sources/5_3_Shadows/5_3_LightDepthBuffer.cpp
@@ -212,7 +212,8 @@ int main()
         glm::mat4 lightProjection, lightView;
         glm::mat4 lightSpaceMatrix;
         float near_plane = 1.0f, far_plane = 7.5f;
-        lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+        // lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+        lightProjection = glm::perspective(glm::radians(75.0f), 1.0f, near_plane, far_plane);
         lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
         lightSpaceMatrix = lightProjection * lightView;
         // render scene from light's point of view
